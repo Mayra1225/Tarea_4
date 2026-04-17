ad4b131bd9f5e8c6f8afde0d7ab50439519eb736
Mayra1225 <mayra.vargas@udla.edu.ec>
2026-04-16 21:06:26 -0500

    correcion

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IAlumnoService, AlumnoService>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("cors_app", policy =>
@@ -13,17 +15,24 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.UseCors("cors_app");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Alumnos v1");
+    c.RoutePrefix = "swagger"; 
+});
 
 app.UseRouting();
+
+app.UseCors("cors_app");
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.Run();
